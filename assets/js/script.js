caf1b24373db245c07495dc1b69e7425496cbdf2
unknown <alcione.silva304@hotmail.com>
2020-10-27 13:38:59 -0300

    LandingPage - Puro

---
+++ assets/js/script.js
@@ -0,0 +1,88 @@
+/*
+Por Tag: getElementByTagName()
+por Id: getElementById()
+por nome: getElementsByName()
+por Class: getElementsByClassName()
+por Seletor: querySelector()
+
+*/
+
+let nome = window.document.getElementById('nome')
+let email = document.querySelector('#email')
+let horario = document.querySelector('#horario')
+let assunto = document.querySelector('#assunto')
+let nomeOk = false
+let emailOk = false
+let horarioOk = false
+let assuntoOk = false
+let mapa = document.querySelector ('#mapa')
+
+nome.style.width = '100%'
+email.style.width = '100%'
+horario.style.width = '100%'
+
+function validaNome () {
+let txtNome = document.querySelector('#txtNome')
+    if (nome.value.length < 3) {
+        txtNome.innerHTML = "Nome Inválido"
+        txtNome.style.color = 'red'
+    } else {
+        txtNome.innerHTML = "Nome Válido"
+        txtNome.style.color = 'green'
+        nomeOk = true
+    }
+}
+
+function validaEmail() {
+    let txtEmail = document.querySelector('#txtEmail')
+    if(email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1) {
+        txtEmail.innerHTML = 'E-mail inválido'
+        txtEmail.style.color = 'red'
+    } else {
+        txtEmail.innerHTML = 'E-mail válido'
+        txtEmail.style.color = 'green' 
+        emailOk = true
+    }
+}
+
+function validaHorario() {
+    let txtHorario = document.querySelector('#txtHorario')
+    if (horario.value.indexOf('/') == -1 || horario.value.indexOf('/') == -1) {
+        txthHorario.innerHTML = 'Data inválida'
+        txtHorario.style.color = 'red'
+    } else {
+        txtHorario.innerHTML = 'Data válida'
+        txtHorario.style.color = 'green' 
+        horarioOk = true
+    }
+}
+
+function validaAssunto () {
+    let txtAssunto = document.querySelector('#txtAssunto')
+    if (assunto.value.length >= 100) {
+        txtAssunto.innerHTML = 'Texto é muito grande, digite no máximo 100 caracteres'
+        txtAssunto.style.color = 'red'
+    } else {
+        txtAssunto.innerHTML = 'Texto válido'
+        txtAssunto.style.color = 'green' 
+        assuntoOk = true
+    }
+    
+}
+
+function enviar() {
+    if (nomeOk == true && emailOk == true && horarioOk == true && assuntoOk == true) {
+        alert ('Formulário enviado com sucesso!')
+      }  else {
+            alert ('Preencha o formulário corretamente antes de enviar..')
+        }
+    }
+
+function mapaZoom () {
+    mapa.style.width = '800px'
+    mapa.style.height = '600px'
+}
+function mapaNormal() {
+    mapa.style.width = '400px'
+    mapa.style.height = '300px'
+}
